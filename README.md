6cd1756ad9b0ce7cf3c28823855e47957de3e24a
Rojin Bijukchhe <ro_jin25@icloud.com>
2024-02-15 11:42:24 -0600

    Dev V.1

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Language-Guessing-App
+
 This app guess's user input language.
+3 NPM Dependencies - Colors , Franc and Langs.
--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-console.log("Hello World!");
+const franc = require("franc");
+const colors = require("colors");
+const langs = require("langs");
+
+const input = process.argv[2];
+const langCode = franc(input);
+
+if (langCode == "und") {
+  console.log(`Sorry, Couldnot Figure it out! Please Try again.`.red);
+} else {
+  console.log(`Language Code: ${langCode.green}`);
+  const language = langs.where("3", langCode);
+  console.log(`Language Guess: ${language.name.underline.bgGreen}`);
+}
+
+// node app "سه نوع و سطح از پروژه که شما را تبدیل به برنامه نویس میکند"
